c51cecdef7267411e89716a6dc87a95d29b60b04
thanhdan0811 <thanhdannguyen@deevotech.com>
2022-02-11 17:07:39 +0700

    adding files

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./search-box.style.css";
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <input
+      onChange={handleChange}
+      className="search"
+      type="search"
+      placeholder={placeholder}
+    />
+  );
+};
